209853a75fdc3575efc4d3fac805e08186338a53
Loïc CAILLÉ <148439946+l0l0o@users.noreply.github.com>
2024-04-29 17:02:13 +0200

    init project

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once __DIR__ . '/parts/header.php'
+?>
+
+<div class="container">
+    <h1>Cook It !</h1>
+    <form action="scripts/signIn.php">
+        <input type="text" class="form-control" placeholder="Enter username" name="userName">
+        <input type="submit">
+    </form>
+
+</div>
